83188815db270cefa65d184442dc9e8944caa2da
Jeremy Fluhmann <jeremy.fluhmann@powerschool.com>
2022-05-26 15:27:57 -0500

    set version to 3.75.99

--- version/version.go
+++ version/version.go
@@ -1,4 +1,5 @@
 package version
 
 // ProviderVersion is set during the release process to the release version of the binary
-var ProviderVersion = "dev"
+// var ProviderVersion = "dev"
+var ProviderVersion = "3.75.99"
